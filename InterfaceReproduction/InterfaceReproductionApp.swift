307de53d9b8c11bcc8d5b94619ec64f702b6d946
zaid ragab <zragab@lcs.on.ca>
2022-11-16 11:26:00 -0500

    Initial Commit

---
+++ InterfaceReproduction/InterfaceReproductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductionApp.swift
+//  InterfaceReproduction
+//
+//  Created by Zaid Ragab on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
